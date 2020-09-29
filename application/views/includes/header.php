7e812ca28be9427d97b5ec664f73bf8214310553
Kevin Ochieng <kevincliffo@gmail.com>
2020-09-29 13:00:37 +0300

    Cleanup

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -283,7 +283,7 @@
                                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <span class="mr-2 d-none d-lg-inline text-gray-600 small">
                                     <?php
-                                        echo $this->session->userdata('FirstName'). ' '.  $this->session->userdata('LastName');
+                                        echo $this->session->userdata('LastName');
                                     ?>
                                     </span>
                                     <!-- <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60" /> -->
